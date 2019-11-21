b8a13e5124ee67b0eb21569ca81fc1308043723d
Arthur <arlrcode@gmail.com>
2019-11-21 13:23:26 +0100

    Correction orthographe

--- draft_code.sh
+++ draft_code.sh
@@ -2,7 +2,7 @@
 
 #Creat en directory call ... in tmp directory and open code in the temp directory to creat a draft code
 #Then open visual studio code
-echo "Nom du broullion : "
+echo "Nom du brouillon : "
 read name
 
 mkdir /tmp/$name
